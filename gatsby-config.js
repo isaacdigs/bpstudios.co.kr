d361fccdbc1f7cfef71e9c027612e20723dc3764
isaacdigs <pk07057@gmail.com>
2019-02-26 16:29:27 +0900

    added yaya's picture

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "bp studios. Content Creators.",
+    author: "Isaac Chung",
+    description: "A website for bp studios."
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
+        name: 'bpstudios',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/bp.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
